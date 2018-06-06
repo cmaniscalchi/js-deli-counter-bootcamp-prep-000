fb9bae404deef6e7058e671fa9f65d6a67c3a1c3
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-06 18:58:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   var array = [];
-   
   for (let i = 0; i < katzDeliLine.length;  i++)
-   {array.push(i+1 + ". " + katzDeliLine[i])}
+  {array.push(i+1 + ". " + katzDeliLine[i])}
   if (katzDeliLine.length === 0) {return `The line is currently empty.`}
   else {return `The line is currently: ${array.join(", ")}`}
 }
